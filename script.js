7bcd681d3fd3f61778abbd0d4f4a35f39abde29a
TravonteD <TravonteD@users.noreply.github.com>
2017-05-30 22:13:31 -0400

    Add the item object constructor

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /* This will be the Javascript for the Shopping Cart Assignment*/
+
+//This is the constructor function for the item objects
+function Item(name, price){
+  this.name = name;
+  this.price = price;
+}
+
+//The code for creating the items will go here
+item1 = new Item();
+item2 = new Item();
+item3 = new Item();
+
+//The Items recieve there properties here
+item1.name = "item1";
+item1.price = 1;
